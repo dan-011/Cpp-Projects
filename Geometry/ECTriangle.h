5f35f1b4f7baf0ae4295ab2b2764f27218d294d4
Daniel Paliulis <89788409+dan-011@users.noreply.github.com>
2022-11-01 17:11:15 -0400

    Update ECTriangle.h

--- Geometry/ECTriangle.h
+++ Geometry/ECTriangle.h
@@ -1,43 +1,30 @@
 //
 //  ECTriangle.h
 //  
-//
 
 #ifndef ECTriangle_h
 #define ECTriangle_h
 
 #include "ECLineSegment.h"
+#include "ECAbstractConvexPolygon.h"
 
 // -----------------------------------------------------------------------------
 // Triangle on 2D plane
 
-class ECTriangle
+class ECTriangle : public ECAbstractConvexPolygon
 {
 public:
     ECTriangle(const EC2DPoint &p1, const EC2DPoint &p2, const EC2DPoint &p3);
     
+    // Test if the polygon is convex? Return false if not
+    virtual bool IsConvex() const;
+    
     // Get area of the triangle
-    double GetArea() const;
-    
-    // Test if a point is inside the triangle (if on the edge, consider it is inside)
-    bool IsPointInside(const EC2DPoint &pt) const;
-    
-    // Test if the passed-in triangle is contained within this triangle
-    // again, if the triangle (rhs) has vertex on the side of this triangle, it is allowed (considered to be contained)
-    bool IsContaining(const ECTriangle &rhs) const;
-    
-    // Test if two triangles intersects (i.e. has non-empty common area)
-    bool IsIntersecting(const ECTriangle &rhs) const;
-    
-private:
-    EC2DPoint point1;
-    EC2DPoint point2;
-    EC2DPoint point3;
+    virtual double GetArea() const;
 
-    ECLineSegment l1;
-    ECLineSegment l2;
-    ECLineSegment l3;
-    // your code goes here
+private:
+    
+    // your code here if needed
 };
 
 
